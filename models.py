34bdf0b1040af6215fcc85e642eaf34aca48be02
Ori98 <neonpabs@gmail.com>
2025-03-06 18:09:32 -0500

    Ensure that this is workign with sqlite

--- models.py
+++ models.py
@@ -5,4 +5,4 @@ class Employee(SQLModel, table=True):
     id: UUID = Field(primary_key=True, default_factory=lambda: uuid4().bytes)
     name: str
     age: int | None
-    is_active: bool # added new field
+    # is_active: bool # added new field
